e586972b8b462958734eade3d4659d0fd5654718
Riley Benson <ryguybenson@yahoo.com>
2019-01-18 09:18:08 -0800

    add jscripts page

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+
+  var question1 = $("#first").val();
+  var question2 = $("#second").val();
+  var question3 = $("#third").val();
+  var question4 = $("#fourth").val();
+  var question5 = $("#fifth").val();
+
+
+
+
+  event.preventDefault();
+  });
+});
