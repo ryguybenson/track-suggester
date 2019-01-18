322ef38a2fcf0b937508e63cde2e0de0dc0d4173
Riley Benson <ryguybenson@yahoo.com>
2019-01-18 15:21:29 -0800

    link image to ruby section

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
+
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-
-  var name = $("#name").val();
-  $(".name").text(name);
-
   var question1 = $("#first").val();
   var question2 = $("#second").val();
   var question3 = $("#third").val();
   var question4 = $("#fourth").val();
   var question5 = $("#fifth").val();
+  var name = $("#name").val();
+  $(".name").text(name);
   if ((question1 === "yes" && question2 === "yes") || (question1 === "yes" && question2 === "no" && question3 === "no")) {
     $("#css").fadeIn();
     $("#ruby").hide();
